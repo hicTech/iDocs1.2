d94c9c611dec68e26370db9ae4220285f2d41a79
marco <marco.allori@gmail.com>
2014-09-18 10:58:13 +0200

    riportata icon iDocs

--- platforms/ios/www/js/local.js
+++ platforms/ios/www/js/local.js
@@ -55,9 +55,9 @@ if(false){ //lanParser == "EN"
 	var INFO_SESSIONE="Richiedi password solo al <b>riavvio completo</b> dell'app:";
     var MIGRATION_MESSAGE = "salva i tuoi documenti per la nuova versione di iDocs";
     var PROCEED = "Procedi";
-    var LANDING_MASSAGE = "Stiamo lavorando alla nuova versione di iDocs!</br>salva i tuoi documenti seguendo la procedura.</br>La procedura creerà la cartella 'iDocs 2.0' sul TUO spazio Dropbox e salverà lì tutti i tuoi documenti. I tuoidocumenti saranno criptati e al sicuro nella TUA cartella Dropbox pronti ad essere importati nella versione diiDocs.</br><b>Se non segui questa procedura e istalli iDocs 2.0 perderai tutti i tuoi documenti.</b>"
+    var LANDING_MASSAGE = "Stiamo lavorando alla nuova versione di iDocs (picDocs)!</br>salva i tuoi documenti seguendo la procedura.</br>La procedura creerà la cartella 'iDocs 2.0' sul TUO spazio Dropbox e salverà lì tutti i tuoi documenti. I tuoi documenti saranno criptati e al sicuro nella TUA cartella Dropbox pronti ad essere importati nella versione di iDocs (picDocs).</br><b>Se non segui questa procedura e istalli picDocs perderai tutti i tuoi documenti.</b>"
     var CONOSCI_DROPBOX = "Non conosci Dropbox?";
-    var CREA_ACCOUNT = "Crea un account </br> (dura 20 secondi)";
+    var CREA_ACCOUNT = "Crea un account </br> (in 20 secondi)";
     var UTENTE = "Utente Dropbox";
     var SPAZIO_LIBERO = "Spazio disponibile";
     var LOGIN_DROPBOX = "Login Dropbox";
@@ -66,7 +66,7 @@ if(false){ //lanParser == "EN"
     var DA_SALVARE_DU_DROPBOX = "File da salvare su Dropbox";
     var DA_ELIMINARE_DA_DROPBOX = "File da eliminare da Dropbox";
     var SINCRONIZZATI = "Avvia procedura di sincronizzazione";
-    var SEI_SINCRONIZZATO = "Tutti i tuoi file sono al sicuro du Dropox pronti per essere importati in iDocs 2.0 </br></br> Seguici su twitter per rimanere aggiornato sulla pubblicazione di iDocs 2.0";
+    var SEI_SINCRONIZZATO = "Tutti i tuoi file sono al sicuro du Dropox pronti per essere importati in iDocs 2.0 </br></br> Seguici su twitter per rimanere aggiornato sulla pubblicazione di picDocs";
     var SEGUI_HICTECH = "Segui @hicTech";
 	var NO_INTERNET = "Non sei connesso a internet!";
 }else{
@@ -103,9 +103,9 @@ if(false){ //lanParser == "EN"
 	var INFO_SESSIONE="Request password only on <b>definitely app killing</b>:";
     var MIGRATION_MESSAGE = "backup your data for new iDocs version";
     var PROCEED = "Proceed";
-    var LANDING_MASSAGE = " We are working on iDocs new version!Please backup your data by following this procedure.</br>It will create an 'iDocs 2.0' folder on YOUR Dropbox where all your photos will be encrypted and stored ready to be imported in iDocs new version.</br><b>If you don't follow this procedure and install iDocs 2.0 you'll lose all your data.</b>"
+    var LANDING_MASSAGE = " We are working on iDocs new version (picDocs)! Please backup your data by following this procedure.</br>It will create an 'iDocs 2.0' folder on YOUR Dropbox where all your photos will be encrypted and stored ready to be imported in iDocs new version (picDocs).</br><b>If you don't follow this procedure and install picDocs you'll lose all your data.</b>"
     var CONOSCI_DROPBOX = "Don't know Dropbox?";
-    var CREA_ACCOUNT = "Create an account </br> (it costs 20 seconds)";
+    var CREA_ACCOUNT = "Create an account </br> (in 20 seconds)";
     var UTENTE = "Dropbox user";
     var SPAZIO_LIBERO = "Available space";
     var LOGIN_DROPBOX = "Login Dropbox";
@@ -114,7 +114,7 @@ if(false){ //lanParser == "EN"
     var DA_SALVARE_DU_DROPBOX = "Files to upload on Dropbox";
     var DA_ELIMINARE_DA_DROPBOX = "File to remove from Dropbox";
     var SINCRONIZZATI = "Synchronize with Dropbox";
-    var SEI_SINCRONIZZATO = "All your files are on your Dropbox folder ready to be imported in iDocs 2.0</br></br> Follow us on twitter to get all updates about iDocs 2.0"
+    var SEI_SINCRONIZZATO = "All your files are on your Dropbox folder ready to be imported in picDocs</br></br> Follow us on twitter to get all updates about picDocs"
     var SEGUI_HICTECH = "Follow @hicTech";
     var NO_INTERNET = "No internet connection!";
 
